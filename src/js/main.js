8c2d68393c26c512d3ac213c2c22d931e3ae668a
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-09-26 13:45:51 +0200

    first anim done

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,29 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+const titleElement = document.querySelector(".logo");
+const txtElement = document.querySelector("h2");
+
+const tl1 = gsap.timeline();
+
+tl1.fromTo(
+  titleElement,
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    duration: 2,
+    delay: 0.5,
+  }
+);
+tl1.fromTo(
+  txtElement,
+  { opacity: 0 },
+  {
+    opacity: 1,
+    duration: 2,
+  },
+  "-=0.1"
+);
