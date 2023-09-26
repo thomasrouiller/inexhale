3540681a269f5fa987424ef6986f97023c2e91f5
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-09-26 14:08:44 +0200

    merci chat gpt

--- src/js/main.js
+++ src/js/main.js
@@ -5,26 +5,73 @@ gsap.registerPlugin(ScrollTrigger);
 
 const titleElement = document.querySelector(".logo");
 const txtElement = document.querySelector("h2");
+const firstCardElement = document.querySelector(".screen-1");
+const firstElement = document.querySelector(".title");
+const secondCardElement = document.querySelector(".screen-2");
+const secondElement = document.querySelector(".timer");
+const startButton = document.querySelector(".start-button");
 
-const tl1 = gsap.timeline();
+const tl2 = gsap.timeline();
 
-tl1.fromTo(
-  titleElement,
-  {
-    opacity: 0,
-  },
-  {
-    opacity: 1,
-    duration: 2,
-    delay: 0.5,
-  }
-);
-tl1.fromTo(
-  txtElement,
-  { opacity: 0 },
-  {
-    opacity: 1,
-    duration: 2,
-  },
-  "-=0.1"
-);
+function animateTl1() {
+  const tl1 = gsap.timeline();
+  tl1.fromTo(
+    titleElement,
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 2,
+      delay: 0.5,
+    }
+  );
+  tl1.fromTo(
+    txtElement,
+    { opacity: 0 },
+    {
+      opacity: 1,
+      duration: 2,
+    },
+    "-=0.1"
+  );
+  tl1.fromTo(
+    firstElement,
+    { opacity: 1 },
+    {
+      opacity: 0,
+      duration: 1.5,
+    },
+    "-=1"
+  );
+  tl1.fromTo(
+    firstCardElement,
+    { opacity: 1 },
+    {
+      opacity: 0,
+      duration: 1.5,
+    },
+    "-=0.5"
+  );
+  return tl1;
+}
+
+function animateTl2() {
+  tl2.to(secondElement, { opacity: 0, duration: 1.5 });
+  tl2.to(
+    secondCardElement,
+    {
+      opacity: 0,
+      duration: 1.5,
+    },
+    "-=0.5"
+  );
+  return tl2;
+}
+
+startButton.addEventListener("click", function () {
+  const tl2 = animateTl2();
+  tl2.play();
+});
+
+const tl1 = animateTl1();
